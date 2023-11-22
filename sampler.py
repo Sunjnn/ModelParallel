f5fbeea569bc2896bb045871b22e9ad399edc93f
Sunjnn <sunjn990@gmail.com>
2023-11-22 16:38:59 +0800

    fix: fix bug in initialization of `DistributedSampler`.
    Transfer `idxs` to GPU before execute `all_reduce` and transfer it
    back to cpu after.

--- sampler.py
+++ sampler.py
@@ -7,22 +7,22 @@ import random
 class DistributedSampler(Sampler):
     """This sampler class is used for distributed computing. Data set is shuffled for each processes."""
 
-    def __init__(self, data_source, group=None) -> None:
+    def __init__(self, data_source, GPU_id, group=None) -> None:
         self.data_source = data_source
         self.group = group
-        self.rand = distributed.get_global_rank(group)
+        self.rank = distributed.get_rank(group)
         self.global_size = distributed.get_world_size(group)
 
         if self.rank == 0:
             # process 0 shuffle a list, other process get the list
             self.idxs = [i for i in range(len(self.data_source))]
             random.shuffle(self.idxs)
-            self.idxs = torch.tensor(self.idxs, dtype=torch.int)
+            self.idxs = torch.tensor(self.idxs, dtype=torch.int).to(GPU_id)
         else:
-            self.idxs = torch.zeros(len(self.data_source), dtype=torch.int)
+            self.idxs = torch.zeros(len(self.data_source), dtype=torch.int).to(GPU_id)
 
-        distributed.barrier(group)
         distributed.all_reduce(self.idxs)
+        self.idxs = self.idxs.to("cpu")
 
     def __len__(self):
         return len(self.data_source)
